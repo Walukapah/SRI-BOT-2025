4dd0762cea1c2f114482f09fc11dc0069f333b67
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-07-20 04:55:36 +0530

    Update youtube.js

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -105,8 +105,7 @@ cmd({
                         downloadUrl = audio128k.url;
                         type = { 
                             audio: { url: downloadUrl }, 
-                            mimetype: "audio/mpeg",
-                            fileName: fileName
+                            mimetype: "audio/mpeg"
                         };
                         break;
                     case "1.2":
@@ -115,8 +114,7 @@ cmd({
                         downloadUrl = audio48k.url;
                         type = { 
                             audio: { url: downloadUrl }, 
-                            mimetype: "audio/mpeg",
-                            fileName: fileName
+                            mimetype: "audio/mpeg"
                         };
                         break;
                     
